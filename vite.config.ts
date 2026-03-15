08829a53ae89c3ab67e1f3ad160c7463e10f8fc1
PotatoInTheBox <ivangusachenko3@gmail.com>
2026-03-14 22:42:43 -0700

    add workflow for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
+  base: '/SS14-Chemistry-Resource-Calculator/',
   root: '.',
   plugins: [react()],
   server: {
